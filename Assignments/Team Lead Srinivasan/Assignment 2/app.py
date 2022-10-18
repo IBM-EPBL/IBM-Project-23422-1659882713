e2017c14f167bd4261cdeb15530c6fd9e64d6f1e
Srinivasan-MN <srinivasan20012019@gmail.com>
2022-10-18 07:37:22 +0530

    Commit

--- Assignments/Team Lead Srinivasan/Assignment 2/app.py
+++ Assignments/Team Lead Srinivasan/Assignment 2/app.py
@@ -1,5 +1,8 @@
 from re import template
-from flask import Flask, render_template
+from sys import exec_prefix
+from urllib import request
+from flask import Flask, render_template, request, redirect, session
+import sqlite3 as sql
 
 app = Flask(__name__)
 
@@ -21,3 +24,40 @@ def about():
 @app.route('/')
 def index():
     return render_template('index.html')    
+
+
+app.route('/addrec',methods = ['POST','GET'])
+def addrec():
+    if request.method == 'POST':
+        try:
+            email = request.form['email']
+            password = request.form['password']
+            address = request.form['address']
+            city = request.form['city']
+            state = request.form['state']
+            zip = request.form['zip']
+            
+            with sql.connect("students.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO students(email,password,address,city,state,zip) VALUES (?,?,?,?,?,?)",(email,password,address,city,state,zip))
+                con.commit
+                msg = "Record Added"
+        except:
+            con.rollback()
+            msg = "Error in insert operation"
+        finally:
+            return render_template("list.html",msg=msg)
+            con.close()    
+
+@app.route('/list')
+def list():
+    con = sql.connect("students.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execute("select * from students")
+
+    students = cur.fetchall()
+    return render_template("list.html", students = students)
+if __name__ == '__main__':
+    app.run(debug = True)    
--- Assignments/Team Lead Srinivasan/Assignment 2/sqlite_de_setup.py
+++ Assignments/Team Lead Srinivasan/Assignment 2/sqlite_de_setup.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('students.db')
 print("opened data base successfully")
 
-conn.execute('CREATE TABLE user1 (email TEXT, password TEXT, address TEXT, city TEXT, state TEXT, zip TEXT)')
+conn.execute('CREATE TABLE students (email TEXT, password TEXT, address TEXT, city TEXT, state TEXT, zip TEXT)')
 
 print("Table created")
 conn.close()
